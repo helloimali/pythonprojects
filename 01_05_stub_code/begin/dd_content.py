a512259f809254100ec5af9ca7d28b4d61b47665
Ali Malik <helloimali@users.noreply.github.com>
2025-04-04 18:23:37 +0000

    get weather function impl

--- 01_05_stub_code/begin/dd_content.py
+++ 01_05_stub_code/begin/dd_content.py
@@ -1,5 +1,7 @@
 import csv
 import random
+import json 
+from urllib import request
 
 def get_random_quote(quote='quotes.csv'):
     try:
@@ -17,8 +19,29 @@ def get_random_quote(quote='quotes.csv'):
     return random.choice(quotes)
     
 
-def get_weather_forecast():
-    pass
+def get_weather_forecast(lat=44.34, lon=10.99):
+    apiKey = "ffb26b4db39c43679fbfb3f7b5ab20b0"
+    apiUrl = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}"
+    data = json.load(request.urlopen(apiUrl))
+    # data now holds weather data in json
+    # print(data)
+    retData = {
+        'city': data['city']['name'],
+        'country': data['city']['country'],
+        'period': list(),
+        }
+    # print(retData["city"])
+
+    # now we need the next 5 days
+    for period in data["list"]:
+        retData["period"].append(
+            {
+             'temp': period["main"]["temp"],
+             'desc': period["weather"][0]["description"],
+            }
+        )
+    # print(retData)
+    return retData
 
 def get_twitter_trends():
     pass
@@ -28,7 +51,9 @@ def get_wikipedia_article():
 
 if __name__ == '__main__':
     # pass # test code
-    print("get random quote")
-    print(get_random_quote())
-    print(get_random_quote())
-    print(get_random_quote(quote=None))
+
+    # print("get random quote")
+    # print(get_random_quote())
+    # print(get_random_quote())
+    # print(get_random_quote(quote=None))
+    print(get_weather_forecast())
