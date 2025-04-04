bf8112c7088f3fecfa72024a171aad9865a325bf
Ali Malik <helloimali@users.noreply.github.com>
2025-04-04 06:55:04 +0000

    get_random_quote

--- 01_05_stub_code/begin/dd_content.py
+++ 01_05_stub_code/begin/dd_content.py
@@ -1,5 +1,21 @@
-def get_random_quote():
-    pass
+import csv
+import random
+
+def get_random_quote(quote='quotes.csv'):
+    try:
+        with open(quote) as csvfile:
+            quotes = [{
+                'author': line[0],
+                'quote': line[1]} for line in csv.reader(csvfile, delimiter='|')]
+            
+    except Exception as e:
+        quotes = [{
+                'author': 'Andy',
+                'quote': 'Vibes are vibing'}
+                ]
+
+    return random.choice(quotes)
+    
 
 def get_weather_forecast():
     pass
@@ -11,4 +27,8 @@ def get_wikipedia_article():
     pass
 
 if __name__ == '__main__':
-    pass # test code
+    # pass # test code
+    print("get random quote")
+    print(get_random_quote())
+    print(get_random_quote())
+    print(get_random_quote(quote=None))
